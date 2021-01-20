2fabdfdf58da07ff7e925d1e3f9f3eae82fae6c6
Eliana Zarzar <eliana.zarzar@proglove.de>
2021-01-20 15:31:13 +0000

    Code cleanup

--- models/customers.sql
+++ models/customers.sql
@@ -4,25 +4,9 @@ with customers as (
 
 ),
 
-orders as (
-
-    select * from {{ ref('stg_orders') }}
-
-),
-
 payments as (
 
     select * from {{ ref('orders') }}
-
-),
-
-customer_amount as (
-    select
-        payments.customer_id,
-        sum(case when payments.status = 'success' then payments.amount end) as total_amount
-
-    from payments
-    group by 1
 
 ),
 
@@ -33,9 +17,11 @@ customer_orders as (
 
         min(order_date) as first_order_date,
         max(order_date) as most_recent_order_date,
-        count(order_id) as number_of_orders
+        count(order_id) as number_of_orders,
 
-    from orders
+        sum(case when payments.status = 'success' then payments.amount end) as total_amount
+
+    from payments
 
     group by 1
 
@@ -51,11 +37,10 @@ final as (
         customer_orders.first_order_date,
         customer_orders.most_recent_order_date,
         coalesce(customer_orders.number_of_orders, 0) as number_of_orders,
-        customer_amount.total_amount as lifetime_value
+        customer_orders.total_amount as lifetime_value
 
     from customers
     left join customer_orders using (customer_id)
-    left join customer_amount using (customer_id)
 
 )
 
